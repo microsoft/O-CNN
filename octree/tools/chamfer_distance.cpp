dce8e944cc95752512322879b9fc06e85228c0a9
wang-ps <wangps@hotmail.com>
2020-12-05 21:19:39 +0800

    Add octree_bbox and update chamfer_distance

--- octree/tools/chamfer_distance.cpp
+++ octree/tools/chamfer_distance.cpp
@@ -1,42 +1,40 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <nanoflann.hpp>
+#include <random>
 #include <string>
 #include <vector>
-#include <random>
-#include <ctime>
-#include <nanoflann.hpp>
 
-#include "points.h"
-#include "filenames.h"
 #include "cmd_flags.h"
+#include "filenames.h"
+#include "points.h"
 
 using namespace std;
 
-DEFINE_string(path_a, kRequired, "", "The file path of a");
-DEFINE_string(path_b, kRequired, "", "The file path of b");
+DEFINE_string(filenames_a, kRequired, "", "The filenames  of a");
+DEFINE_string(filenames_b, kRequired, "", "The filenames of b");
 DEFINE_string(filename_out, kRequired, "", "The output filename");
 DEFINE_int(pt_num, kOptional, -1, "The point num");
-DEFINE_string(category, kOptional, "", "The output filename");
+// DEFINE_string(category, kOptional, "", "The output filename");
 
-
-template < class VectorType, int DIM = 3, typename num_t = float,
-    class Distance = nanoflann::metric_L2, typename IndexType = size_t >
+template <class VectorType, int DIM = 3, typename num_t = float,
+          class Distance = nanoflann::metric_L2, typename IndexType = size_t>
 class KDTreeVectorOfVectorsAdaptor {
  public:
   typedef KDTreeVectorOfVectorsAdaptor<VectorType, DIM, num_t, Distance> self_t;
   typedef typename Distance::template traits<num_t, self_t>::distance_t metric_t;
-  typedef nanoflann::KDTreeSingleIndexAdaptor< metric_t, self_t, DIM, IndexType>  index_t;
+  typedef nanoflann::KDTreeSingleIndexAdaptor<metric_t, self_t, DIM, IndexType>  index_t;
 
   //! The kd-tree index for the user to call its methods as usual with any other FLANN index.
   index_t* index_;
 
  private:
-  const VectorType &data_;
+  const VectorType& data_;
 
  public:
   // Constructor: takes a const ref to the vector of vectors object with the data points
-  KDTreeVectorOfVectorsAdaptor(const VectorType &mat, const int leaf_max_sz = 10)
-    : data_(mat) {
+  KDTreeVectorOfVectorsAdaptor(const VectorType& mat, const int leaf_max_sz = 10)
+      : data_(mat) {
     assert(mat.info().pt_num() != 0);
     index_ = new index_t(DIM, *this, nanoflann::KDTreeSingleIndexAdaptorParams(leaf_max_sz));
     index_->buildIndex();
@@ -47,26 +45,27 @@ class KDTreeVectorOfVectorsAdaptor {
   }
 
   // Query for the num_closest closest points to a given point .
-  inline void query(const num_t *query_point, const size_t num_closest, IndexType *out_indices,
-      num_t *out_distances_sq, const int nChecks_IGNORED = 10) const {
+  inline void query(const num_t* query_point, const size_t num_closest,
+                    IndexType* out_indices, num_t* out_distances_sq,
+                    const int nChecks_IGNORED = 10) const {
     nanoflann::KNNResultSet<num_t, IndexType> resultSet(num_closest);
     resultSet.init(out_indices, out_distances_sq);
     index_->findNeighbors(resultSet, query_point, nanoflann::SearchParams());
   }
 
   // Query for the closest points to a given point (entered as query_point[0:dim-1]).
-  inline void closest(const num_t *query_point, IndexType *out_index, num_t *out_distance_sq) const {
+  inline void closest(const num_t* query_point, IndexType* out_index, num_t* out_distance_sq) const {
     query(query_point, 1, out_index, out_distance_sq);
   }
 
   // Interface expected by KDTreeSingleIndexAdaptor
-  const self_t & derived() const { return *this; }
-  self_t & derived() { return *this; }
+  const self_t& derived() const { return *this; }
+  self_t& derived() { return *this; }
 
   // Must return the number of data points
   inline size_t kdtree_get_point_count() const {
-    return data_.info().pt_num();
-  }
+    return data_.info().pt_num(); 
+    }
 
   // Returns the j'th component of the i'th point in the class:
   inline num_t kdtree_get_pt(const size_t i, int j) const {
@@ -75,11 +74,11 @@ class KDTreeVectorOfVectorsAdaptor {
 
   // Return false to default to a standard bbox computation loop.
   template <class BBOX>
-  bool kdtree_get_bbox(BBOX & /*bb*/) const { return false; }
+  bool kdtree_get_bbox(BBOX& /*bb*/) const { return false; }
 };
 
-void closet_pts(vector<size_t>& idx, vector<float>& distance,
-    const Points& pts, const Points& des) {
+void closet_pts(vector<size_t>& idx, vector<float>& distance, const Points& pts,
+                const Points& des) {
   // build a kdtree
   KDTreeVectorOfVectorsAdaptor<Points> kdtree(des);
 
@@ -88,12 +87,11 @@ void closet_pts(vector<size_t>& idx, vector<float>& distance,
   distance.resize(num);
   idx.resize(num);
   const float* pt = pts.ptr(PointsInfo::kPoint);
-  #pragma omp parallel for
+#pragma omp parallel for
   for (int i = 0; i < num; ++i) {
     kdtree.closest(pt + 3 * i, idx.data() + i, distance.data() + i);
   }
 }
-
 
 std::default_random_engine generator_(static_cast<unsigned int>(time(nullptr)));
 
@@ -130,7 +128,7 @@ void downsample(Points& point_cloud, int target_num) {
 int main(int argc, char* argv[]) {
   bool succ = cflags::ParseCmd(argc, argv);
   if (!succ) {
-    cflags::PrintHelpInfo("\nUsage: update_octree.exe");
+    cflags::PrintHelpInfo("\nUsage: chamfer_distance");
     return 0;
   }
 
@@ -139,10 +137,12 @@ int main(int argc, char* argv[]) {
     cout << "Error: Can not open the output file:" << FLAGS_filename_out << endl;
     return 0;
   }
+  outfile << "path_a, filename_a, path_b, filename_b, num_a, num_b, avg_ab, "
+             "avg_ba, chamfer \n";
 
   vector<string> all_files_a, all_files_b;
-  get_all_filenames(all_files_a, FLAGS_path_a + "/*.points");
-  get_all_filenames(all_files_b, FLAGS_path_b + "/*.points");
+  get_all_filenames(all_files_a, FLAGS_filenames_a);
+  get_all_filenames(all_files_b, FLAGS_filenames_b);
   std::cout << "File number a: " << all_files_a.size() << std::endl;
   std::cout << "File number b: " << all_files_a.size() << std::endl;
 
@@ -179,15 +179,22 @@ int main(int argc, char* argv[]) {
     for (auto& d : distance) { avg_ba += d; }
     avg_ba /= (float)distance.size();
 
+    // scale the results 
+    avg_ab *= 1000;
+    avg_ba *= 1000;
+
     // output to file
-    string filename = extract_filename(filename_a); // no suffix
-    cout << "Processing: " + filename + "\n";
-    outfile << FLAGS_category << ", " << filename << ", "
-        << extract_filename(filename_b) << ", "
-        << point_cloud_a.info().pt_num() << ", "
-        << point_cloud_b.info().pt_num() << ", "
-        << avg_ab << ", " << avg_ba << ", "
-        << avg_ab + avg_ba << endl;
+    string name_a = extract_filename(filename_a);
+    string path_a = extract_filename(extract_path(filename_a));
+    string name_b = extract_filename(filename_b);
+    string path_b = extract_filename(extract_path(filename_b));
+    outfile << path_a << ", " << name_a << ", " << path_b << ", " << name_b
+            << ", " << point_cloud_a.info().pt_num() << ", "
+            << point_cloud_b.info().pt_num() << ", " << avg_ab << ", " << avg_ba
+            << ", " << avg_ab + avg_ba << endl;
+
+    string filename = extract_filename(filename_a);  // no suffix
+    cout << "Processing: " + name_a + " <-> " + name_b + "\n";
   }
 
   outfile.close();
@@ -198,18 +205,17 @@ int main(int argc, char* argv[]) {
 #else
 
 int main(int argc, char* argv[]) {
-
   // load points
-  vector<float> feature{ 1, 1, 1 };
-  vector<float> pt_src{ 2.5, 2.5, 2.5, 1, 1, 0, -1, 0, 0 };
-  vector<float> pt_des{ 0, 0, 0, 1, 1, 1, 2, 2, 2 };
+  vector<float> feature{1, 1, 1};
+  vector<float> pt_src{2.5, 2.5, 2.5, 1, 1, 0, -1, 0, 0};
+  vector<float> pt_des{0, 0, 0, 1, 1, 1, 2, 2, 2};
   Points point_cloud_a, point_cloud_b;
   point_cloud_a.set_points(pt_src, vector<float>(), feature);
   point_cloud_b.set_points(pt_des, vector<float>(), feature);
 
   // cloest distance
   vector<size_t> idx;
-  vector<float> distance; // squared distance
+  vector<float> distance;  // squared distance
   closet_pts(idx, distance, point_cloud_a, point_cloud_b);
 
   // average
---
+++ octree/tools/octree_bbox.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include "filenames.h"
+#include "octree.h"
+#include "cmd_flags.h"
+
+using namespace std;
+using cflags::Require;
+
+DEFINE_string(filenames, kRequired, "", "The input filenames");
+
+int main(int argc, char* argv[]) {
+  bool succ = cflags::ParseCmd(argc, argv);
+  if (!succ) {
+    cflags::PrintHelpInfo("\nUsage: octree_bbox");
+    return 0;
+  }
+
+  string file_path = FLAGS_filenames;
+  vector<string> all_files;
+  get_all_filenames(all_files, file_path);
+  std::sort(all_files.begin(), all_files.end());
+
+  for (int i = 0; i < all_files.size(); i += 2) {
+    string filename = extract_filename(all_files[i + 1]);
+    cout << filename << "\n";
+
+    // load octree
+    Octree octree1, octree2;
+    octree1.read_octree(all_files[i]);
+    octree2.read_octree(all_files[i + 1]);
+
+    // update bbox
+    const float* bbmin = octree1.info().bbmin();
+    const float* bbmax = octree1.info().bbmax();
+    octree2.mutable_info().set_bbox(bbmin, bbmax);
+
+    // save octree
+    octree2.write_octree(all_files[i + 1]);
+  }
+
+  return 0;
+}
