38871256c3b0a26193d809209bd4fd7bd04f9a68
rnpatien <36578300+rnpatien@users.noreply.github.com>
2022-06-24 08:56:07 +1000

    test update

--- tensorflow/test/test_all.py
+++ tensorflow/test/test_all.py
@@ -2,20 +2,21 @@ import os
 import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-from test_octree2col import Octree2ColTest
-from test_octree_conv import OctreeConvTest
+#from test_octree2col import Octree2ColTest
+#from test_octree_conv import OctreeConvTest
 from test_octree_deconv import OctreeDeconvTest
-from test_octree_property import OctreePropertyTest
-from test_octree_search import OctreeSearchTest
-from test_octree_linear import OctreeLinearTest
-from test_octree_nearest import OctreeNearestTest
-from test_octree_gather import OctreeGatherTest
-from test_octree_align import OctreeAlignTest
-from test_octree2points import Octree2PointsTest
-from test_points_property import PointsPropertyTest
+#from test_octree_property import OctreePropertyTest
+#from test_octree_search import OctreeSearchTest
+#from test_octree_linear import OctreeLinearTest
+#from test_octree_nearest import OctreeNearestTest
+#from test_octree_gather import OctreeGatherTest
+#from test_octree_align import OctreeAlignTest
+#from test_octree2points import Octree2PointsTest
+#from test_points_property import PointsPropertyTest
 
 
 if __name__ == "__main__":
   os.environ['CUDA_VISIBLE_DEVICES'] = '0'
   with tf.Graph().as_default():
    tf.test.main()
+   aa=1
